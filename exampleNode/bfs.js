564ba6162e776825d9db0a16db0977349468123b
Lucas Vieira <lucasvg@poli.ufrj.br>
2017-09-08 20:37:33 -0300

    Creates file for BFS search

---
+++ exampleNode/bfs.js
@@ -0,0 +1,36 @@
+/**
+ * Syntax
+ * node <this_file_path> <data_structure_type> <graph_file_path> <origin_vertex>
+*/
+
+// load parameters
+var dataStructureType = process.argv[2];
+var graphFilePath     = process.argv[3];
+var origin            = process.argv[4];
+
+// dependencies
+fs = require('fs');
+
+// validations
+if (!graphFilePath) {
+  throw new Error('Missing graphFilePath parameter');
+}
+if (!dataStructureType) {
+  throw new Error('Missing dataStructureType parameter');
+}
+if (!origin) {
+  throw new Error('Missing origin parameter');
+}
+
+fs.readFile(graphFilePath, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  data = data.split('\n');
+
+  var graphModule = require('../libNode/graph.js');
+  var graph = new graphModule.Graph(data, dataStructureType);
+
+  graph.bfs(origin);
+});
